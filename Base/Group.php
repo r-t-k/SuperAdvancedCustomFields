af73bba8ddf04eff9f3f1084b5d99f5f7f90df6d
Administrator <admin@example.com>
2019-05-16 02:31:38 +0000

    add menu order to groups

--- Base/Group.php
+++ Base/Group.php
@@ -7,6 +7,7 @@ class Group {
 	private $args = array();
 	private $location = array();
 	private $hide = array();
+	private $order = 0;
 
 	public static function OptionGroup($key, $title, $option_page){
 		return new Group($key, $title, false,  $option_page);
@@ -15,15 +16,16 @@ class Group {
 		return new Group($key, $title, false,'acf-options-'.$option_page);
 	}
 
-	public static function NewGroup($key, $title, $param, $operator, $value, $hide = false){
-		$group = new Group($key, $title, false, false);
+	public static function NewGroup($key, $title, $param, $operator, $value, $order = 0, $hide = false){
+		$group = new Group($key, $title, $order, $hide, false);
 		$group->location($param, $operator, $value);
 		$group->_build();
 	}
 
-	public function __construct( $key, $title, $hide = false, $option_page = false ) {
+	public function __construct( $key, $title, $order = 0, $hide = false, $option_page = false ) {
 		$this->args['key']   = $key;
 		$this->args['title'] = $title;
+		$this->order = $order;
 		$this->hide($hide);
 		if ( $option_page !== false && $option_page !== true ) {
 			$this->location( 'options_page', '==',  $option_page );
@@ -74,6 +76,7 @@ class Group {
 				),
 			),
 			'hide_on_screen'        => $this->hide,
+			'menu_order' => $this->order,
 		);
 		acf_add_local_field_group( $group );
 	}
--- plugin-loader.php
+++ plugin-loader.php
@@ -14,7 +14,7 @@ function kf_register_required_plugins() {
 		array(
 			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
 			'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
-			'source'             => ABSPATH . 'wp-content/plugins/SACF/acf.zip', // The plugin source.
+			'source'             => ABSPATH . 'wp-content/plugins/SACF/lib/acf.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
